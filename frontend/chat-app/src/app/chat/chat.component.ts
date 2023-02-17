a7d9592cc66b7a7816b2f5e8475a6d2a9e74e3b4
AdanAlvarezV <ing.adanalvarez.valenzuela@gmail.com>
2023-02-17 16:31:30 -0600

    Full App

--- frontend/chat-app/src/app/chat/chat.component.ts
+++ frontend/chat-app/src/app/chat/chat.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Client } from "@stomp/stompjs";
 import * as SockJS from "sockjs-client";
 import { Mensaje } from './models/mensaje';
-
+ 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
